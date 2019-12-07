5b5d27d8afc3c0c48e5a472caee1f5bed41b0af1
Michael Orlitzky <mjo@gentoo.org>
2019-12-06 19:56:19 -0500

    Make text.xml files a dependency for documents.js, update search_index.py
    Updates to any text.xml should trigger a rebuild of documents.js. The
    Makefile is updated to reflect that. And the updated search_index.py
    that takes a list of files as arguments.
    
    (This commit message is written by the committer, not the author.)
    
    Signed-off-by: Göktürk Yüksek <gokturk@gentoo.org>

--- search_index.py
+++ search_index.py
@@ -2,27 +2,23 @@
 # Copyright 2019 Gentoo Authors
 # Distributed under the terms of the GNU GPL version 2 or later
 import json
-import os
+import os.path
 import sys
 import xml.etree.ElementTree as ET
 
-xmlFile = sys.argv[1]
+files = sys.argv[1:]
 documents = []
 url_root = 'https://devmanual.gentoo.org/'
 
-print('var documents = ', end = '')
-for path, dirs, files in os.walk('.'):
-    if xmlFile in files:
-        tree = ET.parse(path + '/' + xmlFile)
-        root = tree.getroot()
-        for chapter in root.findall('chapter'):
-            try:
-                documents.append({"name": chapter.find('title').text,
-                    "text": chapter.find('body').find('p').text,
-                                  "url": url_root + path[+2:] })
-            except:
-                pass
-    if '.git' in dirs:
-        dirs.remove('.git')  # don't visit git directories
+for f in files:
+    tree = ET.parse(f)
+    root = tree.getroot()
+    for chapter in root.findall('chapter'):
+        try:
+            documents.append({"name": chapter.find('title').text,
+                "text": chapter.find('body').find('p').text,
+                 "url": url_root + os.path.dirname(f) + '/'})
+        except AttributeError:
+            pass
 
-print(json.dumps(documents))
+print('var documents = ' + json.dumps(documents) + ';')
