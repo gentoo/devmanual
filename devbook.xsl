gentoo/devmanual

commit 19153057c1dbbc3537dfc9d1ca20a12d69a46c81

devbook.xsl, bin/gen-eclass-html.sh: Link to privacy policy
Bug: https://bugs.gentoo.org/613938
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

devbook.xsl
@@ -658,6 +658,9 @@
                   <li><a href="https://twitter.com/gentoo" title="@Gentoo on Twitter"><span class="fa fa-twitter fa-fw"></span></a></li>
                   <li><a href="https://www.facebook.com/gentoo.org" title="Gentoo on Facebook"><span class="fa fa-facebook fa-fw"></span></a></li>
                 </ul>
+		<div class="text-center">
+		  <a href="https://wiki.gentoo.org/wiki/Foundation:Privacy_Policy">Privacy Policy</a>
+		</div>
               </xsl:if>
             </div>
             <div class="col-xs-10 col-sm-9 col-md-10">

bin/gen-eclass-html.sh
@@ -70,10 +70,15 @@ IFS='' read -r -d '' FOOTER << 'EOF'
             </div>
 </div>
 <div class="row">
-<div class="col-xs-2 col-sm-3 col-md-2"><ul class="footerlinks three-icons">
+<div class="col-xs-2 col-sm-3 col-md-2">
+<ul class="footerlinks three-icons">
 <li><a href="https://twitter.com/gentoo" title="@Gentoo on Twitter"><span class="fa fa-twitter fa-fw"></span></a></li>
 <li><a href="https://www.facebook.com/gentoo.org" title="Gentoo on Facebook"><span class="fa fa-facebook fa-fw"></span></a></li>
-</ul></div>
+</ul>
+<div class="text-center">
+<a href="https://wiki.gentoo.org/wiki/Foundation:Privacy_Policy">Privacy Policy</a>
+</div>
+</div>
 <div class="col-xs-10 col-sm-9 col-md-10">
 <strong>Copyright (C) 2001-2023 Gentoo Authors</strong><br><small>
                 Gentoo is a trademark of the Gentoo Foundation, Inc.

commit 1fa2d305931d6e1593ff5120c427ae05fbf7bf08

devbook.xsl: Whitespace
Fixes: 19153057c1dbbc3537dfc9d1ca20a12d69a46c81
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

devbook.xsl
@@ -658,9 +658,9 @@
                   <li><a href="https://twitter.com/gentoo" title="@Gentoo on Twitter"><span class="fa fa-twitter fa-fw"></span></a></li>
                   <li><a href="https://www.facebook.com/gentoo.org" title="Gentoo on Facebook"><span class="fa fa-facebook fa-fw"></span></a></li>
                 </ul>
-		<div class="text-center">
-		  <a href="https://wiki.gentoo.org/wiki/Foundation:Privacy_Policy">Privacy Policy</a>
-		</div>
+                <div class="text-center">
+                  <a href="https://wiki.gentoo.org/wiki/Foundation:Privacy_Policy">Privacy Policy</a>
+                </div>
               </xsl:if>
             </div>
             <div class="col-xs-10 col-sm-9 col-md-10">