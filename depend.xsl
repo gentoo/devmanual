gentoo/devmanual

commit bf60a8772d3ece409e2fbe0b4dd8b5a5dd7df167

use html output to avoid invalid self closing elements

devbook.xsl
@@ -11,7 +11,7 @@
 <xsl:import href="xsl/lang.highlight.m4.xsl"/>
 <xsl:import href="xsl/lang.highlight.sgml.xsl"/>
 
-<xsl:output method="xml" indent="yes" omit-xml-declaration="yes"/>
+<xsl:output method="html" indent="yes" omit-xml-declaration="yes"/>
 
 <xsl:variable name="newline">
 <xsl:text>

commit 18c1d5bd22f885c90e440ea408700285bb20165d

xsl: Drop the xmlns=xhtml attribute from all stylesheet elements
This caused some HTML elements to contain spurious xmlns attributes,
e.g. in quickstart/index.html:
<span xmlns="http://www.w3.org/1999/xhtml" class="Statement">dodoc</span>

Apparently this is valid in HTML 5, so neither tidy nor the W3C
validator would warn about it (and it went unnoticed since 2015):

"In HTML documents, elements in the HTML namespace may have an xmlns
attribute specified, if, and only if, it has the exact value
'http://www.w3.org/1999/xhtml'."
https://html.spec.whatwg.org/multipage/dom.html#global-attributes

Use single and double quotes consistently. Whitespace fixes.

Fixes: bf60a8772d3ece409e2fbe0b4dd8b5a5dd7df167
Fixes: e918c7dc8268c4a4723ab4b3a35479f4d191a765
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

depend.xsl
@@ -1,9 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet version='1.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform'
-  xmlns:str="http://exslt.org/strings"
-  xmlns:exslt="http://exslt.org/common"
-  extension-element-prefixes="str exslt xsl"
-  exclude-result-prefixes="str exslt xsl">
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:str="http://exslt.org/strings"
+                xmlns:exslt="http://exslt.org/common"
+                extension-element-prefixes="str exslt xsl"
+                exclude-result-prefixes="str exslt xsl">
 
 <xsl:import href="devbook.xsl"/>
 <xsl:output method="text"/>

devbook.xsl
@@ -1,9 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet version='1.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform'
-  xmlns:str="http://exslt.org/strings"
-  xmlns:exslt="http://exslt.org/common"
-  extension-element-prefixes="str exslt xsl"
-  exclude-result-prefixes="str exslt xsl">
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:str="http://exslt.org/strings"
+                xmlns:exslt="http://exslt.org/common"
+                extension-element-prefixes="str exslt xsl"
+                exclude-result-prefixes="str exslt xsl">
 
 <xsl:import href="xsl/str.tokenize.function.xsl"/>
 <xsl:import href="xsl/lang.highlight.c.xsl"/>
@@ -19,8 +20,7 @@
 <xsl:param name="offline" select="0"/>
 
 <xsl:variable name="newline">
-<xsl:text>
-</xsl:text>
+  <xsl:text>&#x0a;</xsl:text>
 </xsl:variable>
 
 <xsl:template match="chapter">
@@ -128,7 +128,7 @@
     <xsl:for-each select="str:tokenize_plasmaroo(., $newline)">
       <xsl:choose>
         <xsl:when test=". = $newline">
-          <xsl:if test="position() != 1"><xsl:value-of select='$newline'/></xsl:if>
+          <xsl:if test="position() != 1"><xsl:value-of select="$newline"/></xsl:if>
           <xsl:if test="$numbering = 'lines' and position() != last()-1">
             <span style="float: left;"><xsl:number format="01"/>:<xsl:text> </xsl:text></span>
           </xsl:if>
@@ -444,7 +444,7 @@
   <xsl:param name="extraction_counting"/>
 
   <xsl:variable name="doc_self" select="concat($path, 'text.xml')"/>
-  <xsl:if test="count(document($doc_self)/guide/include) &gt; 0 and ($depth &lt; $maxdepth or $maxdepth = '0')">
+  <xsl:if test="count(document($doc_self)/guide/include) &gt; 0 and ($depth &lt; $maxdepth or $maxdepth = 0)">
     <xsl:choose>
       <xsl:when test="$extraction_counting = 1">
         <xsl:for-each select="document($doc_self)/guide/include">
@@ -740,7 +740,11 @@
         <script src="https://assets.gentoo.org/tyrian/jquery.min.js"/>
         <script src="https://assets.gentoo.org/tyrian/bootstrap.min.js"/>
         <script src="https://assets.gentoo.org/lunr/lunr.min.js"/>
-        <script>var documentsSrc = "<xsl:value-of select="$relative_path_depth_recursion"/>documents.js"</script>
+        <script>
+          <xsl:text>var documentsSrc = &quot;</xsl:text>
+          <xsl:value-of select="$relative_path_depth_recursion"/>
+          <xsl:text>documents.js&quot;</xsl:text>
+        </script>
         <script src="{$relative_path_depth_recursion}search.js"/>
       </xsl:if>
     </body>

xsl/lang.highlight.c.xsl
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet version='1.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform'
-  xmlns:str="http://exslt.org/strings"
-  xmlns:exslt="http://exslt.org/common"
-  extension-element-prefixes="str exslt xsl"
-  exclude-result-prefixes="str exslt xsl"
-  xmlns="http://www.w3.org/1999/xhtml">
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:str="http://exslt.org/strings"
+                xmlns:exslt="http://exslt.org/common"
+                extension-element-prefixes="str exslt xsl"
+                exclude-result-prefixes="str exslt xsl">
 
   <xsl:template name="lang.highlight.c.subtokenate">
     <xsl:param name="data"/>

xsl/lang.highlight.ebuild.xsl
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet version='1.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform'
-  xmlns:str="http://exslt.org/strings"
-  xmlns:exslt="http://exslt.org/common"
-  extension-element-prefixes="str exslt xsl"
-  exclude-result-prefixes="str exslt xsl"
-  xmlns="http://www.w3.org/1999/xhtml">
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:str="http://exslt.org/strings"
+                xmlns:exslt="http://exslt.org/common"
+                extension-element-prefixes="str exslt xsl"
+                exclude-result-prefixes="str exslt xsl">
 
   <xsl:variable name="lang.highlight.ebuild.qvariable-start">$</xsl:variable>
   <xsl:variable name="lang.highlight.ebuild.variable-start">${</xsl:variable>

xsl/lang.highlight.m4.xsl
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet version='1.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform'
-  xmlns:str="http://exslt.org/strings"
-  xmlns:exslt="http://exslt.org/common"
-  extension-element-prefixes="str exslt xsl"
-  exclude-result-prefixes="str exslt xsl"
-  xmlns="http://www.w3.org/1999/xhtml">
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:str="http://exslt.org/strings"
+                xmlns:exslt="http://exslt.org/common"
+                extension-element-prefixes="str exslt xsl"
+                exclude-result-prefixes="str exslt xsl">
 
   <xsl:template name="lang.highlight.m4.subtokenate">
     <xsl:param name="data"/>

xsl/lang.highlight.make.xsl
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet version='1.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform'
-  xmlns:str="http://exslt.org/strings"
-  xmlns:exslt="http://exslt.org/common"
-  extension-element-prefixes="str exslt xsl"
-  exclude-result-prefixes="str exslt xsl"
-  xmlns="http://www.w3.org/1999/xhtml">
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:str="http://exslt.org/strings"
+                xmlns:exslt="http://exslt.org/common"
+                extension-element-prefixes="str exslt xsl"
+                exclude-result-prefixes="str exslt xsl">
 
   <xsl:template name="lang.highlight.make.subtokenate">
     <xsl:param name="data"/>

xsl/lang.highlight.sgml.xsl
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet version='1.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform'
-  xmlns:str="http://exslt.org/strings"
-  xmlns:exslt="http://exslt.org/common"
-  extension-element-prefixes="str exslt xsl"
-  exclude-result-prefixes="str exslt xsl"
-  xmlns="http://www.w3.org/1999/xhtml">
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:str="http://exslt.org/strings"
+                xmlns:exslt="http://exslt.org/common"
+                extension-element-prefixes="str exslt xsl"
+                exclude-result-prefixes="str exslt xsl">
 
   <xsl:template name="lang.highlight.sgml.subtokenate">
     <xsl:param name="data"/>

xsl/str.tokenize.function.xsl
@@ -5,11 +5,10 @@
 <xsl:stylesheet version="1.0"
                 xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
                 xmlns:func="http://exslt.org/functions"
-                xmlns:exsl="http://exslt.org/common"
+                xmlns:exslt="http://exslt.org/common"
                 xmlns:str="http://exslt.org/strings"
-                extension-element-prefixes="str func exsl xsl"
-                exclude-result-prefixes="str func exsl xsl"
-                xmlns="http://www.w3.org/1999/xhtml">
+                extension-element-prefixes="str func exslt xsl"
+                exclude-result-prefixes="str func exslt xsl">
 
 <func:function name="str:tokenize_plasmaroo">
   <xsl:param name="string" select="''" />
@@ -18,9 +17,9 @@
     <xsl:when test="not($string)">
       <func:result select="/.." />
     </xsl:when>
-    <xsl:when test="not(function-available('exsl:node-set'))">
+    <xsl:when test="not(function-available('exslt:node-set'))">
       <xsl:message terminate="yes">
-        ERROR: EXSLT - Functions implementation of str:tokenize relies on exsl:node-set().
+        ERROR: EXSLT - Functions implementation of str:tokenize relies on exslt:node-set().
       </xsl:message>
     </xsl:when>
     <xsl:otherwise>
@@ -39,7 +38,7 @@
           </xsl:otherwise>
         </xsl:choose>
       </xsl:variable>
-      <func:result select="exsl:node-set($tokens)/*" />
+      <func:result select="exslt:node-set($tokens)/*" />
     </xsl:otherwise>
   </xsl:choose>
 </func:function>