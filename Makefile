gentoo/devmanual

commit deb1580235af51452e4d0f7ccb9e1015c02fcb85

Makefile: Add %.xsd target
Just in case someone would need an XML Schema file. It is not used by
anything in the project, but we get it for free.

Signed-off-by: Ulrich Müller <ulm@gentoo.org>

Makefile
@@ -99,7 +99,7 @@ validate: devbook.rng
 	done
 	@echo "xmllint validation successful"
 
-%.rng: %.rnc
+%.rng %.xsd: %.rnc
 	trang $< $@
 	sed -i -e '2s/^/<!-- Auto-generated from $<; do not edit! -->\n/' $@
 

commit eda0bfd4f94df84ef0376f53236597fddaf38688

Makefile: Use explicit rule for schema files
Fixes: deb1580235af51452e4d0f7ccb9e1015c02fcb85
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

Makefile
@@ -102,7 +102,7 @@ validate: devbook.rng
 	done
 	@echo "xmllint validation successful"
 
-%.rng %.xsd: %.rnc
+devbook.rng devbook.xsd: devbook.rnc
 	trang $< $@
 	sed -i -e '2s/^/<!-- Auto-generated from $<; do not edit! -->\n/' $@
 